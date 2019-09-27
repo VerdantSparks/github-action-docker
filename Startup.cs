6575916cf2664f375653677d3da89a09e4863821
Ferry To <ferry@verdantsparks.io>
2019-09-27 22:35:35 +0800

    Update Startup.cs
    disable https redirection for practice

--- Startup.cs
+++ Startup.cs
@@ -36,7 +36,7 @@ namespace github_action_docker
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
